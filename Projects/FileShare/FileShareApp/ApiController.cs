f4dc26ff3d8acc7fe3752911316db3664ebd69e6
Yohanes Nurcahyo <yoyokits@gmail.com>
2023-04-23 01:58:04 +0200

    EmbedIO working properly

---
+++ Projects/FileShare/FileShareApp/ApiController.cs
@@ -0,0 +1,22 @@
+﻿using EmbedIO;
+using EmbedIO.Routing;
+using EmbedIO.WebApi;
+
+namespace FileShareApp
+{
+    public class ApiController : WebApiController
+    {
+        public ApiController() : base()
+        { }
+
+        [Route(HttpVerbs.Get, "/test")]
+        public int GetTestResponse()
+        {
+
+
+
+            return 12345;
+        }
+    }
+}
+
--- Projects/FileShare/FileShareApp/MainPage.xaml.cs
+++ Projects/FileShare/FileShareApp/MainPage.xaml.cs
@@ -1,24 +1,69 @@
-﻿namespace FileShareApp;
+﻿using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using EmbedIO;
+using EmbedIO.WebApi;
+
+namespace FileShareApp;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    private WebServer Server { get; set; }
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    private bool IsOnline { get; set; }
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+    public MainPage()
+    {
+        InitializeComponent();
+    }
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+    private async void OnOnlineClicked(object sender, EventArgs e)
+    {
+        if (this.IsOnline)
+        {
+            this.Server?.Dispose();
+        }
+        else
+        {
+            // Server must be started, before WebView is initialized,
+            // because we have no reload implemented in this sample.
+            await Task.Factory.StartNew(async () =>
+            {
+                var url = "http://*:8080";
+                this.Server = new WebServer(HttpListenerMode.EmbedIO, url);
+                var assembly = typeof(App).Assembly;
+                this.Server.WithLocalSessionManager();
+                this.Server.WithWebApi("/api", m => m.WithController(() => new ApiController()));
+                this.Server.WithEmbeddedResources("/", assembly, "EmbedIO.Forms.Sample.html");
+                await this.Server.RunAsync();
+            });
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+            var ips = GetLocalIPv4();
+            this.Webview.Reload();
+        }
+
+        this.IsOnline = !this.IsOnline;
+        this.OnlineStatusLabel.Text = this.IsOnline ? "Online" : "Offline";
+        SemanticScreenReader.Announce(this.OnlineStatusLabel.Text);
+    }
+
+    public static string GetLocalIPv4()
+    {
+        var ipAddrList = new List<string>();
+        var interfaces = NetworkInterface.GetAllNetworkInterfaces();
+        foreach (NetworkInterface item in interfaces)
+        {
+            var type = item.NetworkInterfaceType;
+            if (type == NetworkInterfaceType.Ethernet && item.OperationalStatus == OperationalStatus.Up)
+            {
+                foreach (UnicastIPAddressInformation ip in item.GetIPProperties().UnicastAddresses)
+                {
+                    if (ip.Address.AddressFamily == AddressFamily.InterNetwork)
+                    {
+                        ipAddrList.Add(ip.Address.ToString());
+                    }
+                }
+            }
+        }
+        return ipAddrList.First();
+    }
 }
-
