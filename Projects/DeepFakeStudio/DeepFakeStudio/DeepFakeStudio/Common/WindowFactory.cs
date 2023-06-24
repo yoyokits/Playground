59efcd9c73a9e9bb0cf1c5b86e9c17c999c11272
Yohanes Nurcahyo <yoyokits@gmail.com>
2023-06-24 22:56:10 +0200

    - Add ProcessStepId - Rename DeepFakeStudioNewProjectView to OpenVideosView

---
+++ Projects/DeepFakeStudio/DeepFakeStudio/DeepFakeStudio/Common/WindowFactory.cs
@@ -0,0 +1,35 @@
+﻿namespace DeepFakeStudio.Common
+{
+    using System.Windows;
+    using DeepFakeStudio.Controls;
+
+    /// <summary>
+    /// Defines the <see cref="WindowFactory" />.
+    /// </summary>
+    public static class WindowFactory
+    {
+        #region Methods
+
+        /// <summary>
+        /// The ShowDialog.
+        /// </summary>
+        /// <param name="view">The view<see cref="UIElement"/>.</param>
+        /// <param name="title">The title<see cref="string"/>.</param>
+        /// <param name="width">The width<see cref="double"/>.</param>
+        /// <param name="height">The height<see cref="double"/>.</param>
+        public static void ShowDialog(this UIElement view, string title, double width = 1200, double height = 900)
+        {
+            var window = new DialogWindow
+            {
+                Content = view,
+                Width = width,
+                Height = height,
+                Title = title
+            };
+
+            window.ShowDialog();
+        }
+
+        #endregion Methods
+    }
+}
