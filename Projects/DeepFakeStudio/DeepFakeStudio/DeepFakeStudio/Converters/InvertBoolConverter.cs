fec7c4038cd74712fe66d893b480831f53eaeb5d
Yohanes Nurcahyo <yoyokits@gmail.com>
2023-06-24 08:07:16 +0200

    Add MediaElementPlayer for video player - Add Gu.Wpf.Media for MediaPlayerWrapper

---
+++ Projects/DeepFakeStudio/DeepFakeStudio/DeepFakeStudio/Converters/InvertBoolConverter.cs
@@ -0,0 +1,57 @@
+﻿namespace DeepFakeStudio.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    /// <summary>
+    /// Defines the <see cref="InvertBoolConverter" />.
+    /// </summary>
+    public class InvertBoolConverter : IValueConverter
+    {
+        #region Fields
+
+        private static InvertBoolConverter _instance;
+
+        #endregion Fields
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the Instance.
+        /// </summary>
+        public static InvertBoolConverter Instance => _instance ??= new InvertBoolConverter();
+
+        #endregion Properties
+
+        #region Methods
+
+        /// <summary>
+        /// The Convert.
+        /// </summary>
+        /// <param name="value">The value<see cref="object"/>.</param>
+        /// <param name="targetType">The targetType<see cref="Type"/>.</param>
+        /// <param name="parameter">The parameter<see cref="object"/>.</param>
+        /// <param name="culture">The culture<see cref="CultureInfo"/>.</param>
+        /// <returns>The <see cref="object"/>.</returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !(bool)value;
+        }
+
+        /// <summary>
+        /// The ConvertBack.
+        /// </summary>
+        /// <param name="value">The value<see cref="object"/>.</param>
+        /// <param name="targetType">The targetType<see cref="Type"/>.</param>
+        /// <param name="parameter">The parameter<see cref="object"/>.</param>
+        /// <param name="culture">The culture<see cref="CultureInfo"/>.</param>
+        /// <returns>The <see cref="object"/>.</returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !(bool)value;
+        }
+
+        #endregion Methods
+    }
+}
