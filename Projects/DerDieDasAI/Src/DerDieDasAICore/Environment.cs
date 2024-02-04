a5bcf20565af944c7ff3ce93a6a97831618a4b28
yoyokits <yoyokits@gmail.com>
2024-02-04 22:32:07 +0100

    Initial code for DerDieDasDic - Add some classes - Move to Src folder - Add .gitignore for sqlite - Prepare coding style infrastructure - Remove warnings - Add headers - Add some main classes - Add .editorconfig

---
+++ Projects/DerDieDasAI/Src/DerDieDasAICore/Environment.cs
@@ -0,0 +1,36 @@
+﻿// ========================================== //
+// Developer: Yohanes Wahyu Nurcahyo          //
+// Website: https://github.com/yoyokits       //
+// ========================================== //
+
+namespace DerDieDasAICore
+{
+    using System.Diagnostics;
+    using System.Reflection;
+
+    internal static class Environment
+    {
+        #region Constructors
+
+        static Environment()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var fileVersionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
+            Version = fileVersionInfo.ProductVersion;
+            if (!Directory.Exists(RootPath))
+            {
+                Directory.CreateDirectory(RootPath);
+            }
+        }
+
+        #endregion Constructors
+
+        #region Properties
+
+        public static string RootPath { get; set; } = @"C:\Temp\derdiedas";
+
+        public static string Version { get; set; }
+
+        #endregion Properties
+    }
+}
