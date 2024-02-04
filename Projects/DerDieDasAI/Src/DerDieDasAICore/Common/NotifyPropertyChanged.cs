f9d6cf31cc7d1935c750b68696d2eb53cc394da9
yoyokits <yoyokits@gmail.com>
2024-02-04 22:32:07 +0100

    Initial code for DerDieDasDic - Add some classes - Move to Src folder - Add .gitignore for sqlite - Prepare coding style infrastructure - Remove warnings - Add headers - Add some main classes - Add .editorconfig

---
+++ Projects/DerDieDasAI/Src/DerDieDasAICore/Common/NotifyPropertyChanged.cs
@@ -0,0 +1,48 @@
+﻿// ========================================== //
+// Developer: Yohanes Wahyu Nurcahyo          //
+// Website: https://github.com/yoyokits       //
+// ========================================== //
+
+namespace DerDieDasAICore.Common
+{
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    /// <summary>
+    /// Defines the <see cref="NotifyPropertyChanged" />
+    /// Standard INotifyPropertyChanged implementation.
+    /// </summary>
+    public class NotifyPropertyChanged : INotifyPropertyChanged
+    {
+        #region Events
+
+        /// <summary>
+        /// Defines the PropertyChanged
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion Events
+
+        #region Properties
+
+        /// <summary>
+        /// The property changed handler
+        /// </summary>
+        protected PropertyChangedEventHandler PropertyChangedHandler => this.PropertyChanged;
+
+        #endregion Properties
+
+        #region Methods
+
+        /// <summary>
+        /// The OnPropertyChanged
+        /// </summary>
+        /// <param name="propertyName">The <see cref="string"/></param>
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion Methods
+    }
+}
---
+++ Projects/DerDieDasAI/Src/DerDieDasAICore/Processes/Process.cs
@@ -0,0 +1,30 @@
+﻿// ========================================== //
+// Developer: Yohanes Wahyu Nurcahyo          //
+// Website: https://github.com/yoyokits       //
+// ========================================== //
+
+namespace DerDieDasAICore.Processes
+{
+    using System.ComponentModel;
+
+    public abstract class Process : INotifyPropertyChanged
+    {
+        #region Properties
+
+        public string Name { get; }
+
+        #endregion Properties
+
+        #region Methods
+
+        public abstract void Execute();
+
+        #endregion Methods
+
+        #region Events
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion Events
+    }
+}
