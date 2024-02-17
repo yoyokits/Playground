190d2f854526561ff6ff6f3d86803634e311366a
yoyokits <yoyokits@gmail.com>
2024-02-17 12:20:53 +0100

    Add language classes

---
+++ Projects/DerDieDasAI/Src/DerDieDasAICore/Contracts/Gender.cs
@@ -0,0 +1,16 @@
+﻿// ========================================== //
+// Developer: Yohanes Wahyu Nurcahyo          //
+// Website: https://github.com/yoyokits       //
+// ========================================== //
+
+namespace DerDieDasAICore.Database.Models
+{
+    public enum Gender
+    {
+        Feminine,
+
+        Masculine,
+
+        Neutral
+    }
+}
