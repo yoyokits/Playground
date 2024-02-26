5a593457cb63f681b10ad8a93e877394c0b6faeb
yoyokits <yoyokits@gmail.com>
2024-02-26 19:51:06 +0100

    Scaffolding the translation Scaffold-DbContext "DataSource=D:\de-en.sqlite3" Microsoft.EntityFrameworkCore.Sqlite

---
+++ Projects/DerDieDasAI/Src/DerDieDasAICore/Database/Models/Source/Translation.cs
@@ -0,0 +1,27 @@
+﻿// ========================================== //
+// Developer: Yohanes Wahyu Nurcahyo          //
+// Website: https://github.com/yoyokits       //
+// ========================================== //
+
+namespace DerDieDasAICore.Database.Models.Source;
+
+public partial class Translation
+{
+    #region Properties
+
+    public int? IsGood { get; set; }
+
+    public string Lexentry { get; set; }
+
+    public int? Score { get; set; }
+
+    public string Sense { get; set; }
+
+    public string SenseNum { get; set; }
+
+    public string TransList { get; set; }
+
+    public string WrittenRep { get; set; }
+
+    #endregion Properties
+}
