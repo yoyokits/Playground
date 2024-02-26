50c5f09599521b20c8ca541744ee2cda6c69a14f
yoyokits <yoyokits@gmail.com>
2024-02-26 19:51:06 +0100

    Scaffolding the translation Scaffold-DbContext "DataSource=D:\de-en.sqlite3" Microsoft.EntityFrameworkCore.Sqlite

---
+++ Projects/DerDieDasAI/Src/DerDieDasAICore/Database/Models/Source/DeEnContext.cs
@@ -0,0 +1,76 @@
+﻿// ========================================== //
+// Developer: Yohanes Wahyu Nurcahyo          //
+// Website: https://github.com/yoyokits       //
+// ========================================== //
+
+using Microsoft.EntityFrameworkCore;
+
+namespace DerDieDasAICore.Database.Models.Source;
+
+public partial class DeEnContext : DbContext
+{
+    #region Constructors
+
+    public DeEnContext()
+    {
+    }
+
+    public DeEnContext(DbContextOptions<DeEnContext> options)
+        : base(options)
+    {
+    }
+
+    #endregion Constructors
+
+    #region Properties
+
+    public virtual DbSet<TranslationGrouped> TranslationGroupeds { get; set; }
+
+    public virtual DbSet<Translation> Translations { get; set; }
+
+    #endregion Properties
+
+    #region Methods
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlite("DataSource=de-en.sqlite3");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Translation>(entity =>
+        {
+            entity
+                .HasNoKey()
+                .ToTable("translation");
+
+            entity.Property(e => e.IsGood).HasColumnName("is_good");
+            entity.Property(e => e.Lexentry).HasColumnName("lexentry");
+            entity.Property(e => e.Score).HasColumnName("score");
+            entity.Property(e => e.Sense).HasColumnName("sense");
+            entity.Property(e => e.SenseNum).HasColumnName("sense_num");
+            entity.Property(e => e.TransList).HasColumnName("trans_list");
+            entity.Property(e => e.WrittenRep).HasColumnName("written_rep");
+        });
+
+        modelBuilder.Entity<TranslationGrouped>(entity =>
+        {
+            entity
+                .HasNoKey()
+                .ToView("translation_grouped");
+
+            entity.Property(e => e.Lexentry).HasColumnName("lexentry");
+            entity.Property(e => e.MinSenseNum).HasColumnName("min_sense_num");
+            entity.Property(e => e.Score).HasColumnName("score");
+            entity.Property(e => e.SenseList).HasColumnName("sense_list");
+            entity.Property(e => e.TransList).HasColumnName("trans_list");
+            entity.Property(e => e.WrittenRep).HasColumnName("written_rep");
+        });
+
+        ////OnModelCreatingPartial(modelBuilder);
+    }
+
+    ////partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+
+    #endregion Methods
+}
---
+++ Projects/DerDieDasAI/Src/DerDieDasAICore/Database/Models/Source/TranslationGrouped.cs
@@ -0,0 +1,25 @@
+﻿// ========================================== //
+// Developer: Yohanes Wahyu Nurcahyo          //
+// Website: https://github.com/yoyokits       //
+// ========================================== //
+
+namespace DerDieDasAICore.Database.Models.Source;
+
+public partial class TranslationGrouped
+{
+    #region Properties
+
+    public string Lexentry { get; set; }
+
+    public string MinSenseNum { get; set; }
+
+    public double? Score { get; set; }
+
+    public string SenseList { get; set; }
+
+    public byte[] TransList { get; set; }
+
+    public string WrittenRep { get; set; }
+
+    #endregion Properties
+}
