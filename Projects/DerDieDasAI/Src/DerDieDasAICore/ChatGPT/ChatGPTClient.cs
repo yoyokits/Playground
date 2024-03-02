63a1379611e569538114708be8ecb6b243e318b5
yoyokits <yoyokits@gmail.com>
2024-03-02 21:19:48 +0100

    Prepare ChatGPT infrastructure and unit test - Tidy up the binary output folders - ChatGPT can not be used due to too old resgitration

---
+++ Projects/DerDieDasAI/Src/DerDieDasAICore/ChatGPT/ChatGPTClient.cs
@@ -0,0 +1,66 @@
+﻿// ========================================== //
+// Developer: Yohanes Wahyu Nurcahyo          //
+// Website: https://github.com/yoyokits       //
+// ========================================== //
+
+namespace DerDieDasAICore.ChatGPT
+{
+    using OpenAI_API;
+    using OpenAI_API.Chat;
+    using OpenAI_API.Models;
+
+    public class ChatGPTClient
+    {
+        #region Properties
+
+        public static ChatGPTClient Instance { get; private set; }
+
+        // Replace 'YOUR_API_KEY' with your actual API key
+        public string APIKey { get; }
+
+        // Replace 'YOUR_MESSAGE' with the message you want to send to ChatGPT
+        public string Message { get; set; }
+
+        public OpenAIAPI OpenAIAPI { get; }
+
+        private Conversation DefaultConverstation { get; }
+
+        #endregion Properties
+
+        #region Constructors
+
+        private ChatGPTClient(string apiKey)
+        {
+            APIKey = apiKey;
+            this.OpenAIAPI = new OpenAIAPI(APIKey);
+            this.DefaultConverstation = this.CreateConversation();
+        }
+
+        #endregion Constructors
+
+        #region Methods
+
+        public static ChatGPTClient CreateInstance(string apiKey)
+        {
+            Instance = new ChatGPTClient(apiKey);
+            return Instance;
+        }
+
+        public Task<string> Ask(string message)
+        {
+            /// give instruction as System
+            this.DefaultConverstation.AppendSystemMessage(message);
+            return this.DefaultConverstation.GetResponseFromChatbotAsync();
+        }
+
+        public Conversation CreateConversation()
+        {
+            var chat = OpenAIAPI.Chat.CreateConversation();
+            chat.Model = Model.ChatGPTTurbo;
+            chat.RequestParameters.Temperature = 0;
+            return chat;
+        }
+
+        #endregion Methods
+    }
+}
---
+++ Projects/DerDieDasAI/Src/DerDieDasAITest/Core/ChatGPT/ChatGPTClientTest.cs
@@ -0,0 +1,40 @@
+﻿// ========================================== //
+// Developer: Yohanes Wahyu Nurcahyo          //
+// Website: https://github.com/yoyokits       //
+// ========================================== //
+
+namespace DerDieDasAITest.Core.ChatGPT
+{
+    using DerDieDasAICore.ChatGPT;
+    using DerDieDasAICore.Properties;
+    using FluentAssertions;
+    using System.Diagnostics;
+
+    [TestClass]
+    public class ChatGPTClientTest
+    {
+        #region Methods
+
+        [TestMethod]
+        public async Task Request()
+        {
+            var key = Settings.Default.ChatGPTKey;
+            var chatGPT = ChatGPTClient.CreateInstance(key);
+            var response = string.Empty;
+            try
+            {
+                response = await chatGPT.Ask("Was bedeutet das Haus auf English?");
+                response.Should().NotBeEmpty();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(response);
+                Console.WriteLine(e);
+            }
+
+            Settings.Default.Save();
+        }
+
+        #endregion Methods
+    }
+}
