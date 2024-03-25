a8d84137ecb3aa8535d82f89cafbb1043ed421c3
Yohanes Wahyu Nurcahyo <yoyokits@gmail.com>
2024-03-25 01:17:18 +0100

    Add Stable Diffusion AI client

---
+++ Projects/DerDieDasAI/Src/DerDieDasAITest/Core/AI/StableDiffusionClientTest.cs
@@ -0,0 +1,29 @@
+﻿// ========================================== //
+// Developer: Yohanes Wahyu Nurcahyo          //
+// Website: https://github.com/yoyokits       //
+// ========================================== //
+
+namespace DerDieDasAITest.Core.AI
+{
+    using DerDieDasAICore.AI;
+    using FluentAssertions;
+
+    [TestClass]
+    public class StableDiffusionClientTest
+    {
+        #region Methods
+
+        [TestMethod]
+        public void Text2imgapiSdapiV1Txt2imgPost()
+        {
+            var root = @"http://127.0.0.1:7860/";
+            var client = new StableDiffusionClient(root);
+            var img = client.StableDiffusionProcessingTxt2Img;
+            img.Prompt = "Cat";
+            var response = client.DefaultApi.Text2imgapiSdapiV1Txt2imgPost(img);
+            response.Should().NotBeNull();
+        }
+
+        #endregion Methods
+    }
+}
