98b3a74127215b268941cf3de5e47ab8ff7dd464
Yohanes Wahyu Nurcahyo <yoyokits@gmail.com>
2024-03-25 01:17:18 +0100

    Add Stable Diffusion AI client

---
+++ Projects/DerDieDasAI/Src/DerDieDasAICore/AI/StableDiffusionClient.cs
@@ -0,0 +1,29 @@
+﻿// ========================================== //
+// Developer: Yohanes Wahyu Nurcahyo          //
+// Website: https://github.com/yoyokits       //
+// ========================================== //
+
+namespace DerDieDasAICore.AI
+{
+    using DerDieDasAICore.AI.Model;
+
+    public class StableDiffusionClient
+    {
+        #region Constructors
+
+        public StableDiffusionClient(string root = @"http://127.0.0.1:7860/")
+        {
+            this.DefaultApi = new Api.DefaultApi(root);
+        }
+
+        #endregion Constructors
+
+        #region Properties
+
+        public Api.DefaultApi DefaultApi { get; }
+
+        public StableDiffusionProcessingTxt2Img StableDiffusionProcessingTxt2Img { get; } = new StableDiffusionProcessingTxt2Img();
+
+        #endregion Properties
+    }
+}
