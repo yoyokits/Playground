0b8b644b8ba1274fbc412a20085f00048a8bdd5a
Yohanes Wahyu Nurcahyo <yoyokits@gmail.com>
2024-03-25 01:17:18 +0100

    Add Stable Diffusion API client.

--- Projects/DerDieDasAI/Src/DerDieDasAICore/AI/StableDiffusionClient.cs
+++ Projects/DerDieDasAI/Src/DerDieDasAICore/AI/StableDiffusionClient.cs
@@ -5,6 +5,9 @@
 
 namespace DerDieDasAICore.AI
 {
+    using System.Drawing;
+    using System.Drawing.Imaging;
+    using System.Text;
     using DerDieDasAICore.AI.Model;
 
     public class StableDiffusionClient
@@ -25,5 +28,31 @@ namespace DerDieDasAICore.AI
         public StableDiffusionProcessingTxt2Img StableDiffusionProcessingTxt2Img { get; } = new StableDiffusionProcessingTxt2Img();
 
         #endregion Properties
+
+        #region Methods
+
+        public void Save(string filename, TextToImageResponse txt2imgResponse)
+        {
+            byte[] data = Convert.FromBase64String(txt2imgResponse.Images[0]);
+            var metaData = txt2imgResponse.Info;
+            using (var stream = new MemoryStream(data, 0, data.Length))
+            {
+                var image = Image.FromStream(stream);
+                this.AddMetadata(image, metaData);
+                image.Save(filename, ImageFormat.Jpeg);
+            }
+        }
+
+        private void AddMetadata(Image image, string metaData)
+        {
+            var propItem = image.PropertyItems[0];
+            propItem.Id = 0x9286; // PropertyTagExifUserComment
+            propItem.Type = 2; // ASCII
+            propItem.Value = Encoding.UTF8.GetBytes(metaData + '\0');
+            propItem.Len = propItem.Value.Length;
+            image.SetPropertyItem(propItem);
+        }
+
+        #endregion Methods
     }
 }
