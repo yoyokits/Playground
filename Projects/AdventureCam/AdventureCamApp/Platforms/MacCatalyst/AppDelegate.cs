10bd8da601d7ececf7c8f300fb3f77826e00a098
Yohanes Wahyu Nurcahyo <yoyokits@gmail.com>
2024-09-04 00:00:58 +0200

    Adventure Camera initial commit.

---
+++ Projects/AdventureCam/AdventureCamApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AdventureCamApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
